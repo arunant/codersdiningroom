c93492cf5739f31e5f5ab514e5ac0ab1decd0b52
arunant <1667059216@qq.com>
2017-11-29 13:09:34 +0800

    first

---
+++ src/main/java/com/benny/codersdiningroom/CodersdiningroomApplication.java
@@ -0,0 +1,12 @@
+package com.benny.codersdiningroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersdiningroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodersdiningroomApplication.class, args);
+	}
+}
---
+++ src/main/java/com/benny/codersdiningroom/controller/OrderController.java
@@ -0,0 +1,4 @@
+package com.benny.codersdiningroom.controller;
+
+public class OrderController {
+}
