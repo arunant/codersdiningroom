723e191554c6f1d9f1ac445ffe24c64c6d10e5ec
benny <1667059216@qq.com>
2017-11-29 22:46:30 +0800

    类中的逻辑代码块

--- src/main/java/com/benny/codersdiningroom/entity/test.java
+++ src/main/java/com/benny/codersdiningroom/entity/test.java
@@ -3,9 +3,14 @@ package com.benny.codersdiningroom.entity;
 public class test {
     private String sss;
 
-    String aa ="";
-    int bb =8;
-    if(true){
-        bb=9;
+    String aa = "";
+    int bb = 8;
+
+    //类中的逻辑代码块
+    {
+        if (true) {
+            bb = 9;
+        }
     }
+
 }
