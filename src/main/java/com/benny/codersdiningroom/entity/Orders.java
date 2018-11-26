8cab4b8a7a45c1a6cef18f586fdf717fdd258720
arunant <1667059216@qq.com>
2018-11-26 18:48:52 +0800

    id

--- src/main/java/com/benny/codersdiningroom/entity/Orders.java
+++ src/main/java/com/benny/codersdiningroom/entity/Orders.java
@@ -1,11 +1,14 @@
 package com.benny.codersdiningroom.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Orders {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer orderNO;
 
     public Orders() {
