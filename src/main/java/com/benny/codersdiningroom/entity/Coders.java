300f2adab5018a28dc3ab1d52eea7b2b21b47435
arunant <1667059216@qq.com>
2018-11-26 18:49:21 +0800

    id

--- src/main/java/com/benny/codersdiningroom/entity/Coders.java
+++ src/main/java/com/benny/codersdiningroom/entity/Coders.java
@@ -1,11 +1,14 @@
 package com.benny.codersdiningroom.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Coders {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String phone;
