ec250a8d3b9d89f63966d4c414e4b0506f10d7b9
arunant <1667059216@qq.com>
2018-11-27 19:08:36 +0800

    id

--- src/main/java/com/benny/codersdiningroom/controller/UserController.java
+++ src/main/java/com/benny/codersdiningroom/controller/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * @Auther: HANBIN
  * @Date: 2018-11-26 17:18
@@ -19,12 +21,20 @@ public class UserController {
     private TUserRepository tUserRepository;
     @Autowired
     private SignInRecordRepository signInRecordRepository;
-    @RequestMapping(value = "/adduser")
+    @RequestMapping(value = "/cdr/adduser")
     public TUser addUser(TUser user){
         return tUserRepository.saveAndFlush(user);
     }
-    @RequestMapping(value = "/signin")
+    @RequestMapping(value = "/cdr/login")
+    public String login(){
+        return "";
+    }
+    @RequestMapping(value = "/cdr/signin")
     public SignInRecord signIn(SignInRecord record){
         return signInRecordRepository.saveAndFlush(record);
+    }
+    @RequestMapping(value = "/cdr/getsigninrecord")
+    public List<SignInRecord> getSignInRecord(String date){
+        return signInRecordRepository.findAll();
     }
 }
